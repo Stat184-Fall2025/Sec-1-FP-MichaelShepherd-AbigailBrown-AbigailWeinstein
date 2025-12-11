2ab2680abc87655e5e66b3f001c20e42e21edcd9
shephmic77 <131554214+shephmic77@users.noreply.github.com>
2025-12-11 18:29:55 -0500

    Add files via upload

---
+++ NBA data/combine_data.R
@@ -0,0 +1,22 @@
+Data2015$Year <- 2015
+Data2016$Year <- 2016
+Data2017$Year <- 2017
+Data2018$Year <- 2018
+Data2019$Year <- 2019
+Data2020$Year <- 2020
+Data2021$Year <- 2021
+Data2022$Year <- 2022
+Data2023$Year <- 2023
+Data2024$Year <- 2024
+Data2025$Year <- 2025
+
+
+AllNBA <- rbind(
+  Data2015, Data2016, Data2017, Data2018, Data2019,
+  Data2020, Data2021, Data2022, Data2023, Data2024, Data2025
+)
+
+View(AllNBA)
+
+
+
