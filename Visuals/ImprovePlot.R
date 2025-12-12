04b538d78bb0aaab057dd1ccd21b65bffc433f98
weinsteinabi <abiweinstein@gmail.com>
2025-12-11 23:11:44 -0500

    Update vis files
    Updated visualization file, developed models, and added summary tables

--- Visuals/ImprovePlot.R
+++ Visuals/ImprovePlot.R
@@ -1,6 +1,13 @@
 library(ggplot2)
+library(readr)
 
-ggplot(AllNBA, aes(x = ThreePAR, y = W)) +
+#file - develop basic visualizations for factors of interest
+
+#import data 
+allNBA <- read.csv("NBA data/allNBA.csv")
+
+#plot 1 - three-point attempt rate
+ggplot(allNBA, aes(x = ThreePAR, y = W)) +
   geom_point(alpha = 0.6) +
   geom_smooth(method = "lm", se = FALSE, color = "blue") +
   labs(
@@ -10,7 +17,8 @@ ggplot(AllNBA, aes(x = ThreePAR, y = W)) +
   ) +
   theme_minimal(base_size = 14)
 
-ggplot(AllNBA, aes(x = Age, y = W)) +
+#plot 2 - team age
+ggplot(allNBA, aes(x = Age, y = W)) +
   geom_point(alpha = 0.6) +
   geom_smooth(method = "lm", se = FALSE, color = "blue") +
   labs(
@@ -20,7 +28,8 @@ ggplot(AllNBA, aes(x = Age, y = W)) +
   ) +
   theme_minimal(base_size = 14)
 
-ggplot(AllNBA, aes(x = Pace, y = W)) +
+#plot 3 - pace 
+ggplot(allNBA, aes(x = Pace, y = W)) +
   geom_point(alpha = 0.6) +
   geom_smooth(method = "lm", se = FALSE, color = "blue") +
   labs(
