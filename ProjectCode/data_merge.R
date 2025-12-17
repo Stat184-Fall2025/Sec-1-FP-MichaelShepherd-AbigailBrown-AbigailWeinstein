b4143de8e7abfca18b42b2c9b4b6cf625a12e6b6
weinsteinabi <abiweinstein@gmail.com>
2025-12-17 15:31:23 -0500

    Updated code
    Updated file paths for imported data, refined tables, switched to win ratio metric.

--- ProjectCode/data_merge.R
+++ ProjectCode/data_merge.R
@@ -4,17 +4,15 @@ library(dplyr)
 #file - synthesize and clean data to one data frame
 
 #import data
-data2015 <- read.csv("NBA data/2015.csv")
-data2016 <- read.csv("NBA data/2016.csv")
-data2017 <- read.csv("NBA data/2017.csv")
-data2018 <- read.csv("NBA data/2018.csv")
-data2019 <- read.csv("NBA data/2019.csv")
-data2020 <- read.csv("NBA data/2020.csv")
-data2021 <- read.csv("NBA data/2021.csv")
-data2022 <- read.csv("NBA data/2022.csv")
-data2023 <- read.csv("NBA data/2023.csv")
-data2024 <- read.csv("NBA data/2024.csv")
-data2025 <- read.csv("NBA data/2025.csv")
+data2015 <- read.csv("Data/2015.csv")
+data2016 <- read.csv("Data/2016.csv")
+data2017 <- read.csv("Data/2017.csv")
+data2018 <- read.csv("Data/2018.csv")
+data2019 <- read.csv("Data/2019.csv")
+data2022 <- read.csv("Data/2022.csv")
+data2023 <- read.csv("Data/2023.csv")
+data2024 <- read.csv("Data/2024.csv")
+data2025 <- read.csv("Data/2025.csv")
 
 
 #add necessary id columns (year)
@@ -23,8 +21,6 @@ data2016$Year <- 2016
 data2017$Year <- 2017
 data2018$Year <- 2018
 data2019$Year <- 2019
-data2020$Year <- 2020
-data2021$Year <- 2021
 data2022$Year <- 2022
 data2023$Year <- 2023
 data2024$Year <- 2024
@@ -32,17 +28,24 @@ data2025$Year <- 2025
 
 #merge dataframes 
 allNBA <- rbind(
-  data2015, data2016, data2017, data2018, data2019,
-  data2020, data2021, data2022, data2023, data2024, data2025
+  data2015, data2016, data2017, data2018, data2019, data2022, data2023, data2024, data2025
 )
 
 #clean data
 allNBA <- allNBA %>% select(-c(X, X.1, X.2)) %>%  filter(Team != "League Average")
 allNBA$Team <- gsub("\\*", "", allNBA$Team)
 
+#add measure of win ratio
+allNBA <- allNBA %>% group_by(Team, Year) %>% mutate(
+  Total = W + L, 
+  WinR = W/Total
+)
+
 #check data
 View(allNBA)
 
 #save data frame 
-write.csv(allNBA, file = "NBA data/allNBA.csv")
+write.csv(allNBA, file = "Data/allNBA.csv")
 
+#clear workspace
+rm(data2015, data2016, data2017, data2018, data2019, data2022, data2023, data2024, data2025)
--- ProjectCode/stat_modeling.R
+++ ProjectCode/stat_modeling.R
@@ -5,33 +5,32 @@ library(modelsummary)
 #file - identify relationship and impact of factors on team success
 
 #import data
-allNBA <- read.csv("NBA data/allNBA.csv")
+allNBA <- read.csv("Data/allNBA.csv")
 
+#generate correlaation coefficients
 #identify correlation coefficients
-age <- cor(allNBA$W, allNBA$Age)
-tpar <- cor(allNBA$W, allNBA$X3PAr)
-pace <- cor(allNBA$W, allNBA$Pace)
+age <- cor.test(allNBA$WinR, allNBA$Age)
+tpar <- cor.test(allNBA$WinR, allNBA$X3PAr)
+pace <- cor.test(allNBA$WinR, allNBA$Pace)
 
 #combine into a df
 factor <- c("age", "tpar", "pace")
-coef <- c(age, tpar, pace)
-factorcoef <- data.frame(factor, coef)
+coef <- c(age$estimate, tpar$estimate, pace$estimate)
+t <- c(age$statistic, tpar$statistic, pace$statistic)
+p <- c(age$p.value, tpar$p.value, pace$p.value)
+factor <- c("Player Age", "Three-Point Attempts", "Pace Factor")
+factorcoef <- data.frame(factor, coef, t, p)
 
+
+#linear model creation
 #select and rename columns 
-modelNBA <- allNBA %>% select(W, Age, X3PAr, Pace)
-colnames(modelNBA) <- c("Wins", "Age", "ThreePointAtt.", "Pace")
+modelNBA <- allNBA %>% select(WinR, Age, X3PAr, Pace)
+colnames(modelNBA) <- c("Win Rate", "Age", "ThreePointAtt.", "Pace")
 
-#model 
-winmodel1 <- lm(Wins ~ Age + ThreePointAtt. + Pace, data = modelNBA)
+#create model 
+winmodel1 <- lm(`Win Rate` ~ Age + ThreePointAtt. + Pace, data = modelNBA)
 summary(winmodel1)
-
 model <- list("Model 1" = winmodel1)
 
-table3 <- modelsummary(model, stars = TRUE, output = 'kableExtra', 
-                       estimate = c("Coef." = "{estimate}"),
-                       statistic = c("p-Value" = "{p.value}", "Std. Error" = "{std.error}"), 
-                       shape = term ~ model + statistic, 
-                       gof_omit = "R2 Adj.|AIC|BIC|Log.Lik.|F|RMSE",
-                       align = "lccc")
-table3
-
+#clear workspace
+rm(pace, tpar, coef, factor, p, t, age, modelNBA)
